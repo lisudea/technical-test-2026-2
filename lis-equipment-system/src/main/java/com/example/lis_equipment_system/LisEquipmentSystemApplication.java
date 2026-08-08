1f0784d1cb9422120acd845b64f3cf7e316646e4
Karen Jimenez <164250915+JCKaren@users.noreply.github.com>
2026-08-08 17:55:43 -0500

    Springboot project base

---
+++ lis-equipment-system/src/main/java/com/example/lis_equipment_system/LisEquipmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.lis_equipment_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LisEquipmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LisEquipmentSystemApplication.class, args);
+	}
+
+}
---
+++ lis-equipment-system/src/test/java/com/example/lis_equipment_system/LisEquipmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lis_equipment_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LisEquipmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
