e3b89e955fa566ef181ecd28a33be19bb28f92be
Camilo Mosquera <camilomosquerav@gmail.com>
2026-08-09 19:18:12 -0500

    chore: configure application infrastructure

---
+++ equipment-reservation-api/src/main/java/com/lisudea/equipmentreservation/EquipmentReservationApiApplication.java
@@ -0,0 +1,13 @@
+package com.lisudea.equipmentreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipmentReservationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquipmentReservationApiApplication.class, args);
+	}
+
+}
